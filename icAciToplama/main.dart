62f7a6a652c8bb6b60286b9942c0cbdd7485a6e2
Sevda <125583274+sevdaghn@users.noreply.github.com>
2025-01-16 16:53:44 +0300

    Create main.dart

---
+++ icAciToplama/main.dart
@@ -0,0 +1,14 @@
+import 'package:untitled2/sinif.dart';
+
+void main () {
+
+  int toplam1 = Odev4().icAciToplami(4);
+  print("İç açı toplamı: $toplam1");
+
+  int toplam2 = Odev4().icAciToplami(5);
+  print("İç açı toplamı: $toplam2");
+
+  int toplam3 = Odev4().icAciToplami(6);
+  print("İç açı toplamı: $toplam3");
+
+}
