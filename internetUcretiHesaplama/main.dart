5abb5fc272298d0518b724f12c52c520dce327dd
Sevda <125583274+sevdaghn@users.noreply.github.com>
2025-01-18 12:36:35 +0300

    Create main.dart

---
+++ internetUcretiHesaplama/main.dart
@@ -0,0 +1,18 @@
+import 'package:untitled2/sinif.dart';
+
+void main()
+{
+
+  var o7 = Odev7();
+
+  int gelenSonuc = o7.internetUcretiHesaplama(57);
+  print("Hesaplanan Ücret: $gelenSonuc");
+
+  int gelenSonuc2 = o7.internetUcretiHesaplama(45);
+  print("Hesaplanan Ücret: $gelenSonuc2");
+
+  int gelenSonuc3 = o7.internetUcretiHesaplama(50);
+  print("Hesaplanan Ücret: $gelenSonuc3");
+
+
+}
